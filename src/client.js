4aad3df2a3f5b4b63c3e4687f0185e1d30a59457
Vardan Galstyan <vardan.galstjan@gmail.com>
2023-04-10 00:05:49 +0200

    chore: changes the react version

--- src/client.js
+++ src/client.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { hydrateRoot } from 'react-dom/client';
-import App from './app.js';
-import { BrowserRouter} from "react-router-dom";
-import { CookiesProvider } from 'react-cookie';
+import React from "react";
+import { hydrateRoot } from "react-dom";
+import App from "./app.js";
+import { BrowserRouter } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 
 hydrateRoot(
-    document.getElementById('root'),
-    <CookiesProvider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </CookiesProvider>
+  document.getElementById("root"),
+  <CookiesProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </CookiesProvider>
 );
