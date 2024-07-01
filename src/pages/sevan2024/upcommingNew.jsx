f5a3de0a19f5ea9f106eead8d820c5ff481f6b23
Vardan Galstyan <vardan.galstjan@gmail.com>
2024-07-01 17:13:41 +0200

    fix: removes imporotant css tag

--- src/pages/sevan2024/upcommingNew.jsx
+++ src/pages/sevan2024/upcommingNew.jsx
@@ -1,27 +1,22 @@
-import React from "react";
-import { Fragment } from "react";
+import React, { Fragment } from "react";
 import { Helmet } from "react-helmet";
 import FootterNew from "../footterNew/footterNew.jsx";
 
-import Shuttle from "./shuttle.jsx";
-import Programs from "./program.jsx";
-import MustKnow from "./mustknow.jsx";
-import Credo from "./credo.jsx";
-import Platform from "./platform.jsx";
-import Participate from "./Participate/Participate.jsx";
-import Valounteering from "./Volunteering/Volunteering.jsx";
 import Apply from "./Apply/Apply.jsx";
+import Organizers from "./Organizers/Organizers.jsx";
 import Stats from "./Status/index.jsx";
-import Organizers from "./Organizers/Organizers.jsx"
-import Partners from "./Partners/Partners.jsx"
+import Valounteering from "./Volunteering/Volunteering.jsx";
+import Credo from "./credo.jsx";
+import MustKnow from "./mustknow.jsx";
+import Programs from "./program.jsx";
+import Shuttle from "./shuttle.jsx";
 
-import WeOffer from "./WeOffer/index.jsx";
 import Navbar from "./Navbar/index.jsx";
 import { GetAccess } from "./component.min.jsx";
 
 //css
-import "./upcommingNew.css";
 import Roadmap from "./Roadmap/Roadmap.jsx";
+import "./upcommingNew.css";
 
 //Media
 
@@ -30,7 +25,7 @@ class UpcommingNew extends React.Component {
     super(props);
     this.state = {
       screenWidth: null,
-      scrollY: null
+      scrollY: null,
     };
     this.handleResize = this.handleResize.bind(this);
     this.handleScroll = this.handleScroll.bind(this);
@@ -38,16 +33,16 @@ class UpcommingNew extends React.Component {
 
   componentDidMount() {
     this.setState({
-      screenWidth:window.innerWidth,
-      scrollY:window.scrollY
-    })
-    window.addEventListener('resize', this.handleResize);
-    window.addEventListener('scroll', this.handleScroll);
+      screenWidth: window.innerWidth,
+      scrollY: window.scrollY,
+    });
+    window.addEventListener("resize", this.handleResize);
+    window.addEventListener("scroll", this.handleScroll);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize);
-    window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener("resize", this.handleResize);
+    window.removeEventListener("scroll", this.handleScroll);
   }
 
   handleResize() {
@@ -58,9 +53,7 @@ class UpcommingNew extends React.Component {
     this.setState({ scrollY: window.scrollY });
   }
 
-
   render = () => {
-    console.log(this.state)
     return (
       <Fragment>
         <Helmet>
@@ -89,36 +82,44 @@ class UpcommingNew extends React.Component {
         <div className="upcommingNew">
           {/* Part 1 */}
           <div className="upcommingNewTop sevan2023 sevan2024">
-            {(this.state.screenWidth > 1024 || this.state.screenWidth === null) &&
-                <div className= {this.state.scrollY >= 650 ?  "sevan2024-mainNavbar" : "sevan2024-headerNavbar" }>
-                  <Navbar />
-                </div>
-            }
-
+            {(this.state.screenWidth > 1024 ||
+              this.state.screenWidth === null) && (
+              <div
+                className={
+                  this.state.scrollY >= 650
+                    ? "sevan2024-mainNavbar"
+                    : "sevan2024-headerNavbar"
+                }
+              >
+                <Navbar />
+              </div>
+            )}
 
             <div className="wb-topTitle">
-              <h1>A Campsite Festival <br/> for Startup Enthusiasts</h1>
-              <h5>
-                Where Ideas Spark and Dreams Take Flight!
-              </h5>
+              <h1>
+                A Campsite Festival <br /> for Startup Enthusiasts
+              </h1>
+              <h5>Where Ideas Spark and Dreams Take Flight!</h5>
             </div>
             <div className="wb-footerTitle">
-              <h4>SEVAN STARTUP SUMMIT 2024 <br/> JULY 21 - 27</h4>
+              <h4>
+                SEVAN STARTUP SUMMIT 2024 <br /> JULY 21 - 27
+              </h4>
               <div className="wb-applyButtonBlock wb-applyButtonBlock-right">
                 <a
-                    href="https://www.f6s.com/sevan-startup-summit-24/apply"
-                    // onClick={() => setPopupStatus(true)}
-                    target="_blank"
-                    rel="noopener noreferrer">
+                  href="https://www.f6s.com/sevan-startup-summit-24/apply"
+                  // onClick={() => setPopupStatus(true)}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button>Apply as a Startup</button>
                 </a>
-
               </div>
             </div>
-
           </div>
           {/* Part 2 */}
-          {(this.state.screenWidth <= 1024 && this.state.screenWidth !== null) && <Navbar />}
+          {this.state.screenWidth <= 1024 &&
+            this.state.screenWidth !== null && <Navbar />}
           <GetAccess />
           <Stats />
           <Credo ref="credo" />
