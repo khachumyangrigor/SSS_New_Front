e99c6dd6cd2f5920a46ba510430ef8cf26dd6d92
Vardan Galstyan <vardan.galstjan@gmail.com>
2024-08-15 19:23:25 +0200

    chore: adds cjs to output file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import commonjs from "vite-plugin-commonjs";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
+  build: {
+    outDir: "dist/server",
+    ssr: "src/server.jsx",
+    rollupOptions: {
+      output: {
+        format: "cjs",
+        entryFileNames: "[name].cjs",
+      },
+    },
+  },
 });
