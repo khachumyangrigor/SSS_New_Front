8395cb579eed1898f18e80882b388800c1ba4102
Vardan Galstyan <vardan.galstjan@gmail.com>
2024-08-15 23:12:14 +0200

    test: testing vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,19 @@ import commonjs from "vite-plugin-commonjs";
 
 export default defineConfig({
   plugins: [react(), commonjs()],
-  optimizeDeps: {
-    include: ["@vite-mono/lib-cjs"],
-  },
   build: {
-    commonjsOptions: {
-      include: [/lib-cjs/, /node_modules/],
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        server: "src/server.jsx", // Your server entry point
+      },
+      output: {
+        format: "cjs", // CommonJS format
+        entryFileNames: "[name].js", // Ensures server.js is used, not main.cjs
+        dir: "dist/server", // Output directory for server-side code
+      },
     },
+    ssr: true, // Ensure server-side rendering is enabled
   },
-  assetsInclude: ["**/*.PNG"],
+  assetsInclude: ["**/*.PNG"], // Include PNG files
 });
