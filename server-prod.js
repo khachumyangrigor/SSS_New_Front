9b6be3383e49ca0c31647127e2fcbd8956442303
Vardan Galstyan <vardan.galstjan@gmail.com>
2024-08-15 22:43:14 +0200

    test: adds js to cjs

--- server-prod.js
+++ server-prod.js
@@ -15,7 +15,7 @@ app.use(
 app.use("*", async (_, res) => {
   try {
     const template = fs.readFileSync("./dist/client/index.html", "utf-8");
-    const { render } = await import("./dist/server/server.js");
+    const { render } = await import("./dist/server/server.cjs");
 
     const html = template.replace(`<!--outlet-->`, render);
     res.status(200).set({ "Content-Type": "text/html" }).end(html);
